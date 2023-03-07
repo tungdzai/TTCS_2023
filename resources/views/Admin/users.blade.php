2a4a34f07823e944781a630991333914c76cd679
NguyenVietTung <tungnguyen0603202@gmail.com>
2023-03-07 15:38:16 +0700

    PreviewProduct

--- resources/views/Admin/users.blade.php
+++ resources/views/Admin/users.blade.php
@@ -9,21 +9,6 @@
             <span>Users</span></a>
     </li>
 @endsection
-@section('search')
-    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search" method="post"
-          action="">
-        <div class="input-group">
-            @csrf
-            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
-                   aria-label="Search" aria-describedby="basic-addon2" name="search" value="{{session('titleSearch')}}">
-            <div class="input-group-append">
-                <button class="btn btn-primary" type="submit">
-                    <i class="fas fa-search fa-sm"></i>
-                </button>
-            </div>
-        </div>
-    </form>
-@endsection
 @section('content')
     <div class="container-fluid">
         <!-- Page Heading -->
