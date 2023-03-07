417e6e96e2ce4504a5be74853b51dd013f838269
NguyenVietTung <tungnguyen0603202@gmail.com>
2023-03-07 15:59:38 +0700

    PreviewProduct

--- resources/views/Layout/layout.blade.php
+++ resources/views/Layout/layout.blade.php
@@ -13,7 +13,6 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <!-- Custom styles for this template-->
     <link href="../../admin_lte/css/sb-admin-2.min.css" rel="stylesheet">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
     <title>@yield("title")</title>
 </head>
 
@@ -73,9 +72,7 @@
 <script src="../../admin_lte/js/sb-admin-2.min.js"></script>
 <!-- Page level plugins -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" ></script>
-<script src="../../assets/js/preview.js"></script>
 <script src="../../assets/js/previewProduct.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" ></script>
 <script src="../../admin_lte/js/previewAvatar.js"></script>
 </body>
 </html>
