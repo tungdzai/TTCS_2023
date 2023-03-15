e3f788be2ff6ec152f756edc3f3525ec1a7527b6
NguyenVietTung <tungnguyen0603202@gmail.com>
2023-03-15 15:48:01 +0700

    fix cmt 19

--- app/Models/Details.php
+++ app/Models/Details.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * Class Details
+/** Class Details
  * @property integer $order_id
  * @property integer $product_id
  * @property integer $quantity
--- app/Models/Orders.php
+++ app/Models/Orders.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * Class Orders
+/** Class Orders
  * @property integer $customer_id
  * @property integer $quantity
  * @property integer $total
