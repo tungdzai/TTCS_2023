9207856bf1b9044826c6ce8810864b3bbf186f53
NguyenVietTung <tungnguyen0603202@gmail.com>
2023-03-16 09:03:39 +0700

    fix20

--- app/Http/Controllers/Language/LanguageController.php
+++ app/Http/Controllers/Language/LanguageController.php
@@ -3,14 +3,13 @@
 namespace App\Http\Controllers\Language;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-
 class LanguageController extends Controller
 {
     /** get session language
      * @param $language
      * @return \Illuminate\Http\RedirectResponse
      */
+
     public function index($language): \Illuminate\Http\RedirectResponse
     {
         if ($language) {
